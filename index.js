22d9ca059adfbc15fa812a4ed7cfd8c406296f12
Diego-comp-dev <eng.diego.dev@gmail.com>
2026-09-14 16:21:33 -0300

    exercise_two

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 
 // alert(`Olá, bom dia ${nome}`)
 
+let num_one = Number(prompt("Digite o primeiro numero:"))
+let num_two = Number(prompt("Digite o segundo numero:"))
+let soma = num_one + num_two
+alert(`A soma dos numeros é:${soma}`)
