4bec981faa9240664b38d1ff3dda2f5eb1e614d5
Diego-comp-dev <eng.diego.dev@gmail.com>
2026-09-14 16:30:26 -0300

    exercise_three

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 // alert(`Olá, bom dia ${nome}`)
 
-let num_one = Number(prompt("Digite o primeiro numero:"))
-let num_two = Number(prompt("Digite o segundo numero:"))
-let soma = num_one + num_two
-alert(`A soma dos numeros é:${soma}`)
+// let num_one = Number(prompt("Digite o primeiro numero:"))
+// let num_two = Number(prompt("Digite o segundo numero:"))
+// let soma = num_one + num_two
+// alert(`A soma dos numeros é:${soma}`)
+
+let idade = Number(prompt("Digite sua idade:"))
+if (idade <18) {
+    alert("Você é menor de idade")
+} else{
+    alert("Você é maior de idade")
+}
