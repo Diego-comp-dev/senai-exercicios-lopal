517a5f41189c598eba1bbf42dcf5bb105cb00c88
Diego-comp-dev <eng.diego.dev@gmail.com>
2026-09-18 16:39:43 -0300

    exercise four

--- index.js
+++ index.js
@@ -7,9 +7,17 @@
 // let soma = num_one + num_two
 // alert(`A soma dos numeros é:${soma}`)
 
-let idade = Number(prompt("Digite sua idade:"))
-if (idade <18) {
-    alert("Você é menor de idade")
-} else{
-    alert("Você é maior de idade")
+// let idade = Number(prompt("Digite sua idade:"))
+// if (idade <18) {
+//     alert("Você é menor de idade")
+// } else{
+//     alert("Você é maior de idade")
+// }
+
+let num = Number(prompt("Digite um numero"))
+let lastnum = num % 10
+if(lastnum === 0 || lastnum === 2 || lastnum === 4 || lastnum === 6 || lastnum === 8){
+    alert(`seu numero é par porque termina com: ${lastnum}`)
+} else {
+    alert(`seu numero é impar porque termina com: ${lastnum}`)
 }
